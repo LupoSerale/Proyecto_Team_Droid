b82114749fc3ee9fe227f522c7531d963767ee47
LupoSerale <jwcalderon2@misena.edu.co>
2021-11-10 20:47:10 -0500

    Initial commit

---
+++ app/src/main/java/com/luposerale/proyecto_team_droid/Listar_Sitios_Turisticos.kt
@@ -0,0 +1,11 @@
+package com.luposerale.proyecto_team_droid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Listar_Sitios_Turisticos : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_listar_sitios_turisticos)
+    }
+}
---
+++ app/src/main/java/com/luposerale/proyecto_team_droid/Obelisco_Puerto_Lopez.kt
@@ -0,0 +1,11 @@
+package com.luposerale.proyecto_team_droid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Obelisco_Puerto_Lopez : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_obelisco_puerto_lopez)
+    }
+}
